0e638c1509be8d56534954e18af0d435db71945b
sarukiti <sarukiti1891@gmail.com>
2025-11-13 01:28:35 +0000

    クライアントライブラリができたと思われる

--- src/bin/ev3_ros_bridge.rs
+++ src/bin/ev3_ros_bridge.rs
@@ -2,7 +2,9 @@ use std::sync::Arc;
 use tokio::sync::Mutex;
 
 use safe_drive::msg::common_interfaces::geometry_msgs;
-use safe_drive::{context::Context, error::DynError, topic::subscriber::Subscriber};
+use safe_drive::{context::Context, error::DynError, topic::{publisher::Publisher, subscriber::Subscriber}};
+use ev3_ros_msg::msg::Sensor;
+
 use tonic::{transport::Server, Request, Response, Status};
 
 use ev3_ros_bridge::pb;
@@ -10,11 +12,12 @@ use pb::{ev3_ros_bridge_server::Ev3RosBridge, CmdVel, SensorData, WriteResponse}
 
 pub struct Ev3RosBridgeServer {
     subscriber: Arc<Mutex<Subscriber<geometry_msgs::msg::Twist>>>,
+    publisher: Publisher<Sensor>,
 }
 
 impl Ev3RosBridgeServer {
-    fn new(subscriber: Arc<Mutex<Subscriber<geometry_msgs::msg::Twist>>>) -> Ev3RosBridgeServer {
-        Ev3RosBridgeServer { subscriber }
+    fn new(subscriber: Arc<Mutex<Subscriber<geometry_msgs::msg::Twist>>>, publisher: Publisher<Sensor>) -> Ev3RosBridgeServer {
+        Ev3RosBridgeServer { subscriber, publisher }
     }
 }
 
@@ -36,7 +39,13 @@ impl Ev3RosBridge for Ev3RosBridgeServer {
     ) -> Result<Response<WriteResponse>, Status> {
         let metadata_len = request.metadata().len() as i32;
         let _sensor_data = request.into_inner();
-        // TODO: 適当にpublishする
+
+        self.publisher.send(&Sensor {
+            touch_pressed_state: _sensor_data.touch_pressed_state,
+            gyro_angle: _sensor_data.gyro_angle,
+            gyro_rotational_speed: _sensor_data.gyro_rotational_speed,
+            ultrasonic_distance: _sensor_data.ultrasonic_distance,
+        }).unwrap();
         Ok(Response::new(WriteResponse { size: metadata_len }))
     }
 }
@@ -47,8 +56,9 @@ async fn main() -> Result<(), DynError> {
     let node = ctx.create_node("ev3_ros_bridge", None, Default::default())?;
 
     let subscriber = node.create_subscriber::<geometry_msgs::msg::Twist>("/cmd_vel", None)?;
+    let publisher = node.create_publisher::<Sensor>("/sensor_data", None)?;
 
-    let server = Ev3RosBridgeServer::new(Arc::new(Mutex::new(subscriber)));
+    let server = Ev3RosBridgeServer::new(Arc::new(Mutex::new(subscriber)), publisher);
 
     let args: Vec<String> = std::env::args().collect();
     const DEFAULT_ADDRESS: &str = "127.0.0.1:50051";
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
 pub mod pb {
     tonic::include_proto!("ev3_ros_bridge");
 }
+
+use std::cell::RefCell;
+use tonic::Request;
+use pb::{CmdVel, SensorData, WriteResponse};
+
+pub struct TonicHandle {
+    tokio_runtime: tokio::runtime::Runtime,
+    client: RefCell<pb::ev3_ros_bridge_client::Ev3RosBridgeClient<tonic::transport::Channel>>,
+}
+
+impl TonicHandle {
+    pub fn new(url: &str) -> Self {
+        let tokio_runtime = tokio::runtime::Runtime::new().unwrap();
+        let client = tokio_runtime.block_on(async {
+            pb::ev3_ros_bridge_client::Ev3RosBridgeClient::connect(url.to_string())
+                .await
+                .unwrap()
+        });
+        Self {
+            tokio_runtime: tokio_runtime,
+            client: std::cell::RefCell::new(client),
+        }
+    }
+
+    pub fn read_cmd_vel(&self) -> Result<CmdVel, tonic::Status> {
+        let request = Request::new(());
+        self.tokio_runtime.block_on(async {
+            let mut client = self.client.borrow_mut();
+            let response = client.read_cmd_vel(request).await?;
+            Ok(response.into_inner())
+        })
+    }
+
+    pub fn write_sensor_data(&self, sensor_data: SensorData) -> Result<WriteResponse, tonic::Status> {
+        let request = Request::new(sensor_data);
+        self.tokio_runtime.block_on(async {
+            let mut client = self.client.borrow_mut();
+            let response = client.write_sensor_data(request).await?;
+            Ok(response.into_inner())
+        })
+    }
+}

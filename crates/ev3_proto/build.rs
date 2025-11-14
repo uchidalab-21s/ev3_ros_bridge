9f538438405e8fe5f399e2bb3faf74360807410a
kankyouhakai <kankyouhakai096@gmail.com>
2025-11-14 07:41:13 +0000

    tonicのバージョン上げ辛すぎ🥺

--- crates/ev3_proto/build.rs
+++ crates/ev3_proto/build.rs
@@ -1,8 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file_descriptors = protox::compile(["ev3_ros_bridge.proto"], ["proto/"]).unwrap();
-    tonic_build::configure()
-        .build_server(true)
-        .compile_fds(file_descriptors)
-        .unwrap();
+    tonic_prost_build::compile_protos("proto/ev3_ros_bridge.proto")?;
     Ok(())
 }
